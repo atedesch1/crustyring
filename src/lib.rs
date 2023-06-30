b8df6bd2a29f89bcd14ea1b04b1d84fffaab404f
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-06-30 17:16:02 -0300

    add hashring helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,30 @@ pub struct NodeInfo {
     id: u64,
     addr: String,
 }
+
+pub struct HashRing {}
+
+impl HashRing {
+    pub fn distance(a: u64, b: u64) -> u64 {
+        let bigger: u64;
+        let smaller: u64;
+
+        if a > b {
+            bigger = a;
+            smaller = b;
+        } else {
+            bigger = b;
+            smaller = a;
+        }
+
+        std::cmp::min(bigger - smaller, (u64::MAX - bigger) + smaller)
+    }
+
+    pub fn counter_clowise_distance(a: u64, b: u64) -> u64 {
+        if a > b {
+            a - b
+        } else {
+            (u64::MAX - b) + a
+        }
+    }
+}
